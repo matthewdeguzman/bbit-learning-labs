d7bae6fd0a504c023d34212291fb10cdfa16cf92
Matthew DeGuzman <matthew.deguzman0613@gmail.com>
2024-03-18 18:05:27 -0400

    Add producer

---
+++ Tech-Lab-On-Campus/Producer-And-Consumer/producer/solution/producer_sol.py
@@ -0,0 +1,25 @@
+import os
+from producer_interface import mqProducerInterface
+import pika
+
+class mqProducer(mqProducerInterface):
+    
+    def __init__(self, routing_key: str, exchange_name: str) -> None:
+        self.routing_key = routing_key
+        self.exchange_name = exchange_name
+        self.setupRMQConnection()
+
+    def setupRMQConnection(self) -> None:
+        conParams = pika.URLParameters(os.environ['AMQP_URL'])
+        self.connection = pika.BlockingConnection(parameters=conParams)
+        self.channel = self.connection.channel()
+        self.channel.exchange_declare(self.exchange_name)
+        self.queue = self.channel.queue_declare(queue=self.routing_key)
+
+    def publishOrder(self, message: str) -> None:
+        self.channel.basic_publish(exchange=self.exchange_name,
+                      routing_key=self.routing_key,
+                      body=message)
+        print(" [x] Delivered order")
+        self.connection.close()
+        print(" [x] Connection closed")
